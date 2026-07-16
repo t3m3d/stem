eb3597331862d4fe81c3c6ffdc89beb623403ee8
t3m3d <brian@kryptonbytes.com>
2026-07-15 22:52:11 -0400

    windows work

--- windows/Stem.Windows/KryptonTheme.cs
+++ windows/Stem.Windows/KryptonTheme.cs
@@ -40,7 +40,7 @@ public static class KryptonTheme
             new(58, 42, 96), new(139, 92, 246), new(139, 92, 246),
             StemSettings.DefaultAnsiPalette());
 
-    public static void ApplyApplication(string? theme)
+    public static void ApplyApplication(string? theme, double backgroundOpacity = 1)
     {
         if (Application.Current is null)
         {
@@ -49,11 +49,12 @@ public static class KryptonTheme
 
         var resources = Application.Current.Resources;
         var light = IsLight(theme);
-        SetBrush(resources, "KryptonBackgroundBrush", light ? 0xF8F5FF : 0x10081D);
-        SetBrush(resources, "KryptonPanelBrush", light ? 0xF0E9FF : 0x180C2A);
-        SetBrush(resources, "KryptonShellBrush", light ? 0xE9DDFF : 0x260D46);
-        SetBrush(resources, "KryptonTerminalChromeBrush", light ? 0xFFFFFF : 0x07050C);
-        SetBrush(resources, "KryptonFieldBrush", light ? 0xFFFFFF : 0x0B0712);
+        var opacity = Math.Clamp(backgroundOpacity, 0.2, 1);
+        SetBrush(resources, "KryptonBackgroundBrush", light ? 0xF8F5FF : 0x10081D, opacity);
+        SetBrush(resources, "KryptonPanelBrush", light ? 0xF0E9FF : 0x180C2A, opacity);
+        SetBrush(resources, "KryptonShellBrush", light ? 0xE9DDFF : 0x260D46, opacity);
+        SetBrush(resources, "KryptonTerminalChromeBrush", light ? 0xFFFFFF : 0x07050C, opacity);
+        SetBrush(resources, "KryptonFieldBrush", light ? 0xFFFFFF : 0x0B0712, Math.Max(0.72, opacity));
         SetBrush(resources, "KryptonTextBrush", light ? 0x241933 : 0xF0EBFA);
         SetBrush(resources, "KryptonStrongTextBrush", light ? 0x170D26 : 0xFFFFFF);
         SetBrush(resources, "KryptonTitleTextBrush", 0xFFFFFF);
@@ -63,10 +64,10 @@ public static class KryptonTheme
         SetBrush(resources, "KryptonAccentHighlightBrush", light ? 0x6D28D9 : 0xD7C3FF);
         SetBrush(resources, "KryptonBorderBrush", light ? 0xA78BFA : 0x654698);
         SetBrush(resources, "KryptonFrameBrush", light ? 0x7C3AED : 0xB79AFF);
-        SetBrush(resources, "KryptonMenuBrush", light ? 0xFCFAFF : 0x160D25);
+        SetBrush(resources, "KryptonMenuBrush", light ? 0xFCFAFF : 0x160D25, Math.Max(0.9, opacity));
         SetBrush(resources, "KryptonMenuHoverBrush", light ? 0xE9DDFF : 0x3A1D64);
         SetBrush(resources, "KryptonMenuSelectedBrush", light ? 0xDAC8FF : 0x512B82);
-        SetBrush(resources, "KryptonStatusBrush", light ? 0xEDE4FF : 0x170A29);
+        SetBrush(resources, "KryptonStatusBrush", light ? 0xEDE4FF : 0x170A29, opacity);
         SetBrush(resources, "KryptonChromeHoverBrush", light ? 0x30FFFFFF : 0x24FFFFFF);
         SetBrush(resources, "KryptonChromePressedBrush", light ? 0x48FFFFFF : 0x38FFFFFF);
         SetBrush(resources, "KryptonDangerBrush", 0xC63C51);
@@ -74,17 +75,27 @@ public static class KryptonTheme
         SetBrush(resources, "KryptonSelectionBrush", light ? 0xDAC8FF : 0x3A2A60);
         SetBrush(resources, "KryptonShadowBrush", light ? 0x553A235F : 0xAA000000, alphaIncluded: true);
 
-        resources["KryptonShellStartColor"] = ColorValue(light ? 0x7C3AED : 0x4C1D95);
-        resources["KryptonShellMidColor"] = ColorValue(light ? 0x8B5CF6 : 0x6D28D9);
-        resources["KryptonShellEndColor"] = ColorValue(light ? 0xA78BFA : 0x2E1065);
+        resources["KryptonShellStartColor"] = ColorValue(light ? 0x7C3AED : 0x4C1D95, opacity);
+        resources["KryptonShellMidColor"] = ColorValue(light ? 0x8B5CF6 : 0x6D28D9, opacity);
+        resources["KryptonShellEndColor"] = ColorValue(light ? 0xA78BFA : 0x2E1065, opacity);
         resources["KryptonFrameStartColor"] = ColorValue(light ? 0xA78BFA : 0xD7C3FF);
         resources["KryptonFrameEndColor"] = ColorValue(light ? 0x6D28D9 : 0x6D28D9);
         resources["KryptonWavePrimaryColor"] = ColorValue(light ? 0x72FFFFFF : 0x58FFFFFF, alphaIncluded: true);
         resources["KryptonWaveSecondaryColor"] = ColorValue(light ? 0x55E9DDFF : 0x48C4A7FF, alphaIncluded: true);
     }
 
-    private static void SetBrush(ResourceDictionary resources, string key, uint value, bool alphaIncluded = false) =>
+    private static void SetBrush(ResourceDictionary resources, string key, uint value, double opacity = 1) =>
+        resources[key] = new SolidColorBrush(ColorValue(value, opacity));
+
+    private static void SetBrush(ResourceDictionary resources, string key, uint value, bool alphaIncluded) =>
         resources[key] = new SolidColorBrush(ColorValue(value, alphaIncluded));
+
+    private static Color ColorValue(uint value, double opacity) =>
+        Color.FromArgb(
+            (byte)Math.Round(Math.Clamp(opacity, 0, 1) * 255),
+            (byte)(value >> 16),
+            (byte)(value >> 8),
+            (byte)value);
 
     private static Color ColorValue(uint value, bool alphaIncluded = false) => alphaIncluded
         ? Color.FromArgb((byte)(value >> 24), (byte)(value >> 16), (byte)(value >> 8), (byte)value)
